782168a48812046225c6f7cd95830766121ce824
Dillon <loh.dillon@gmail.com>
2025-06-24 14:26:41 +0800

    refactor: move research pubs into data json file

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ module.exports = function(eleventyConfig) {
     // Copy css and static folders to the output directory
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("static");
-  
+    eleventyConfig.addPassthroughCopy("_data");
+    
     return {
       dir: {
         input: ".",
---
+++ _data/research.js
@@ -0,0 +1,27 @@
+module.exports = {
+    publications: [
+      {
+        text: `Loh, D., Bednarz, T., Xia, X., & Guan, F. (2024). AdaVLN: Towards visual language navigation in continuous indoor environments with moving humans [Preprint]. arXiv.`,
+        url: "https://arxiv.org/abs/2411.18539"
+      }
+    ],
+    presentations: [
+      {
+        text: `Loh, D., Takeuchi, T., Cooray, S., & Iwasaki, D. (2022, October). Latent space representations of galaxies and their relationship with galactic properties. Talk presented at the 4th Data Science in Astronomy Workshop, Tokyo, Japan.`,
+        url: "https://github.com/dillonloh/galaxy-dim/blob/main/Data%20Science%20Conference.pptx.pdf",
+        label: "Slides Link"
+      },
+      {
+        text: `Loh, D., Takeuchi, T., Cooray, S., & Iwasaki, D. (2022, September). Application of unsupervised neural networks for predicting galactic properties via image features. Poster presented at the 9th East Asian Numerical Astrophysics Conference, Okinawa, Japan.`,
+        url: "https://github.com/dillonloh/galaxy-dim/blob/main/9th%20EANAM%20Poster%20-%20Dillon.pdf",
+        label: "Poster Link"
+      }
+    ],
+    textbooks: [
+      {
+        text: `Contribution to Chapter 13: [Galaxy Face]. In T. T. Takeuchi, Applications of big data and machine learning in galaxy formation and evolution (pp. 259–284). CRC Press.`,
+        isbn: "ISBN: 0367611392, 9780367611392"
+      }
+    ]
+  };
+  
