78f109091eac087534f840bdcdae8b43e7fb87d7
Dillon <loh.dillon@gmail.com>
2025-06-24 14:29:56 +0800

    refactor: move news to data json

---
+++ _data/news.js
@@ -0,0 +1,15 @@
+module.exports = [
+    {
+      date: "07 Feb 2023",
+      content: `I will be joining the University of Cambridge’s MPhil in Machine Learning and Machine Intelligence in Sep 2025 (Michaelmas 2025)!`
+    },
+    {
+      date: "14 Jan 2025",
+      content: `Alongside Professor Frank Guan (SIT), Professor Xia Xinxing (Shanghai University), and Zhang Mengyu (Shanghai University), we have been awarded the NVIDIA Academic Grant for the period of Feb 2025 - Aug 2025!`
+    },
+    {
+      date: "24 Nov 2024",
+      content: `Preprint for AdaVLN: Towards Visual Language Navigation in Continuous Indoor Environments with Moving Humans is up on arXiv!`
+    }
+  ];
+  
